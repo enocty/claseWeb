ddb4faefc4de2f6fca60157e9d2323d6d22c4212
enocty <joluzao@gmail.com>
2020-05-07 00:57:31 -0500

    añadir errores

--- añadirUsuario.php
+++ añadirUsuario.php
@@ -13,28 +13,29 @@ $ban=mysqli_select_db($link,"videoteca");
 //echo " Password = $pas <br>";
  if($usu==NULL|$pas==NULL|$pas2==NULL|$em==NULL) 
     {
-        echo "un campo est&aacute; vacio.";
-        formRegistro();
+       // echo "un campo est&aacute; vacio.";
+     header("Location: vare.php");
+        //formRegistro();
     }else{
         // ¿Coinciden las contrase&ntilde;as?
         if($pas!=$pas2) {
-            echo "Las contraseñas no coinciden";
-            formRegistro();
+           // echo "Las contraseñas no coinciden";
+             header("Location: conin.php")
+            //formRegistro();
         }else{
             $checkuser = mysqli_query($link,"SELECT * FROM videoteca WHERE usuarios = '$usu'");
-            
             $checkemail = mysqli_query($link,"SELECT * FROM videoteca WHERE email = '$em'");
             $email_exist = mysqli_num_rows($checkemail);
             $username_exist = mysqli_num_rows($checkuser);
     
             if ($email_exist>0|$username_exist>0) {
-                echo "El nombre de usuario o la cuenta de correo estan ya en uso";
-                formRegistro();
+                header("Location: errlog.php");
+                //formRegistro();
             }else{
                mysqli_query($link, "insert into usuarios (id,usuario, password,descripcion,email,tipo,fecha) values ('$id', '$usu','$pas','$des','$em','$tip','$fec')") or die(mysqli_error());
                 echo 'El usuario '.$usu.' ha sido registrado de manera satisfactoria.<br />';
                 echo 'Ahora puede entrar ingresando su usuario y su password <br />';
-
+                header("Location: corlog.php");
                 
             }
         }
---
+++ vare.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<title>Videoteca</title>
+<meta http-equiv="Content-Language" content="English" />
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<link rel="stylesheet" type="text/css" href="style.css" media="screen" />
+<style type="text/css">
+<!--
+.Estilo1 {font-size: 18px}
+-->
+</style>
+</head>
+<body>
+
+<div id="wrap">
+
+<div id="header">
+<h1><a href="#">Videoteca</a></h1>
+<h2>FCC - BUAP </h2>
+</div>
+
+<div id="top"> </div>
+<div id="menu">
+<ul>
+<li><a href="index.php">Incio</a></li>   
+<li><a href="Consultapelicula.php">Consulta películas</a></li>
+<li><a href="Registrarse.php">Registrarse</a></li>
+<li><a href="Acceso.php">Login</a></li>
+<li><a href="Contacto.php">Contacto</a></li>
+</ul>
+</div>
+
+<div id="content">
+<div class="left"> 
+
+<h2><a href="#" class="Estilo1">Formulario de Registro nuevo Cliente </a></h2>
+<div class="articles">
+  <p>&nbsp;</p>
+     <FORM ACTION="añadirUsuario.php" METHOD="POST">
+    
+  Nombre de usuario nuevo: <BR />
+  <INPUT TYPE="TEXT" NAME="USU" REQUIRED />
+  <BR /><BR />
+  Password:<br />
+  <INPUT TYPE="PASSWORD" NAME="PASWD" REQUIRED />
+  <br /><br />
+  Confirma Password:<br />
+  <INPUT TYPE="PASSWORD" NAME="PASWD2" REQUIRED />
+  <br /><br />
+  Email:<br />
+  <INPUT TYPE="EMAIL" NAME="EM" REQUIRED />
+  <br /><br />
+  Error un campo esta vacion 
+  <br /><br />
+ <INPUT TYPE="SUBMIT" NAME="ENVIAR" VALOR=" ENTRAR " />
+  
+  </FORM>
+  <p>&nbsp;</p>
+  <p>&nbsp;</p>
+  <p>&nbsp;</p>
+  <p>&nbsp;</p>
+  <p>&nbsp;</p>
+  <p>&nbsp;</p>
+  <p>&nbsp;</p>
+  <p>&nbsp;</p>
+  <p>&nbsp;</p>
+  <p>&nbsp;</p>
+  <p>&nbsp;</p>
+</div>
+</div>
+
+<div class="right"> 
+
+<h2>Top de peliculas: </h2>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+</div>
+<div style="clear: both;"> </div>
+</div>
+
+<div id="bottom"> </div>
+<div id="footer">
+CSS ejemplo de <a href="http://www.free-css-templates.com/">Free CSS Templates</a></div>
+</div>
+
+</body>
+</html>
