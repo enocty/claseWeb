38d455485195ab198d15ff0fb6e3db3d9aa2ec42
Jose Luis Luna <louislunahz@gmail.com>
2020-05-13 14:35:35 -0500

    ya puse el archivo de grafos y el txt con las calificaciones. Ya sirve

--- claseGrafo.php
+++ claseGrafo.php
@@ -18,54 +18,25 @@
   <link href="vis/dist/vis.css" rel="stylesheet" type="text/css" />
 
 <?php
+//aqui leeremos del archivo calificaciones.txt
+$fp = fopen("calificaciones.txt", "r");
+$i=1;
+while (!feof($fp)){
+    $linea = fgets($fp);
+   // echo $linea;
+    $cadena = explode(" ", $linea);
+    echo "$cadena[0]";
+    echo "<br>";
+    echo "$cadena[1]";
+    echo "<br>";
+    $alumnos[$i]=$cadena[0];
+    $promedio[$i]=floatval($cadena[1]);
+    $i++;
+}
+fclose($fp);
 
-$alumnos[1]='Aplicaciones web';
-$alumnos[2]='Antonio';
-$alumnos[3]='Edgar';
-$alumnos[4]='Jose Luis';
-$alumnos[5]='Ricardo';
-$alumnos[6]='Margarita';
-$alumnos[7]='Marco';
-$alumnos[8]='Fernando';
-$alumnos[9]='Antonio';
-$alumnos[10]='Edgar';
-$alumnos[11]='Jose Luis';
-$alumnos[12]='Ricardo';
-$alumnos[13]='Margarita';
-$alumnos[14]='Marco';
-$alumnos[15]='Ángel';
-$alumnos[16]='Antonio';
-$alumnos[17]='Edgar';
-$alumnos[18]='Jose Luis';
-$alumnos[19]='Ricardo';
-$alumnos[20]='Margarita';
-$alumnos[21]='Marco';
-
-
-$promedio[1]=30;
-$promedio[2]=10;
-$promedio[3]=8.7;
-$promedio[4]=5.7;
-$promedio[5]=7.8;
-$promedio[6]=9.6;
-$promedio[7]=1;
-$promedio[8]=5.5;
-$promedio[9]=10;
-$promedio[10]=8.7;
-$promedio[11]=5.7;
-$promedio[12]=7.8;
-$promedio[13]=9.6;
-$promedio[14]=1;
-$promedio[15]=1.5;
-$promedio[16]=10;
-$promedio[17]=8.7;
-$promedio[18]=5.7;
-$promedio[19]=7.8;
-$promedio[20]=9.6;
-$promedio[21]=1;
-
-
-$numAlumnos=21;
+$numAlumnos=37;
+echo "Numero de alumnos: $numAlumnos";
 
 $masarcos="";
 for($i=1; $i<=$numAlumnos; $i++)
@@ -74,9 +45,21 @@ $arcos="nodes = [".$masarcos."];";
 
 
 $masaristas="";
-for($i=2; $i<=$numAlumnos; $i++)
-$masaristas=$masaristas."{from: $i, to: 1, value: $promedio[$i]/4, label: '$promedio[$i]', font: {color:'red', face: 'times new roman'}},";
+for($i=2; $i<=$numAlumnos; $i++){
+  if ($promedio[$i]>=9) {
+     $masaristas=$masaristas."{from: $i, to: 1, value: $promedio[$i]/4, label: '$promedio[$i]', font: {color:'green', face: 'times new roman'}},";
+  }elseif ($promedio[$i]>=8 && $promedio[$i]>=8.999) {
+    $masaristas=$masaristas."{from: $i, to: 1, value: $promedio[$i]/4, label: '$promedio[$i]', font: {color:'yellow', face: 'times new roman'}},";
+  }elseif ($promedio[$i]>=6 && $promedio[$i]>=7.999) {
+     $masaristas=$masaristas."{from: $i, to: 1, value: $promedio[$i]/4, label: '$promedio[$i]', font: {color:'orange', face: 'times new roman'}},";
+  }else{
+    $masaristas=$masaristas."{from: $i, to: 1, value: $promedio[$i]/4, label: '$promedio[$i]', font: {color:'red', face: 'times new roman'}},";
+  }
+ 
+
+}
 $aristas="edges = [".$masaristas."];";
+
 
 
 
